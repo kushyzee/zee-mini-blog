d617cea047b85db97be59dfaffefb1ef9368dc91
Arinze Ogbuniba <niklaus.oa@gmail.com>
2025-09-23 20:42:54 +0100

    initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Mini Blog",
+  description:
+    "Discover amazing stories, insights, and ideas from our community of writers.",
+};
